7aa0b91696d1b031f866ac4c49c7ba534362110d
Alexandra Podgayskaya <sasha241992pod@gmail.com>
2021-08-30 13:51:25 +0300

    NavBar - add if isAuth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,17 @@ function NavBar() {
     return (
         <Navbar bg="dark" variant="dark" fixed="top">
             <Navbar.Brand style={{ color: "grey" }}>Admin UI</Navbar.Brand>
-            <Nav className="me-auto">
-                <Button>Add new</Button>
-            </Nav>
-            <Nav className="ml-auto">
-                <Nav.Link style={{ color: "white" }}>{login}</Nav.Link>
-                <Nav.Link href="#logout" style={{ color: "white" }}>Logout</Nav.Link>
-            </Nav>
-
+            {isAuth &&
+                <>
+                    <Nav className="me-auto">
+                        <Button>Add new</Button>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Link style={{ color: "white" }}>{login}</Nav.Link>
+                        <Nav.Link href="#logout" style={{ color: "white" }}>Logout</Nav.Link>
+                    </Nav>
+                </>
+            }
         </Navbar>
     )
 }

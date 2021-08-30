a4b49ee50da512c9c6d6b52c19ad9b44934f9e77
Alexandra Podgayskaya <sasha241992pod@gmail.com>
2021-08-30 15:15:53 +0300

    Add Auth component

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Container, Card, Form } from 'react-bootstrap'
 
 function Auth() {
     return (
-        <div>
-            LOGIN
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 600 }} className="p-5">
+                <h2 className="m-auto">Login</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="E-mail"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Password"
+                    />
+                </Form>
+            </Card>
+        </Container>
     )
 }
 
